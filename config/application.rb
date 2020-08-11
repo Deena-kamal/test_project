ee718855db32f895511813ed9debc7db6c91e95c
Deena-kamal <dina.ahmed@trianglz.com>
2020-08-11 12:46:42 +0200

    Add tests to user

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,19 @@ module TestProject
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # don't generate RSpec tests for views and helpers
+    config.generators do |g|
+      g.test_framework :rspec, fixture: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+      g.view_specs false
+      g.helper_specs false
+      g.stylesheets = false
+      g.javascripts = false
+      g.helper = false
+    end
+
+  config.autoload_paths += %W(\#{config.root}/lib)
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe User do
+  before { @user = FactoryGirl.build(:user) }
+
+  subject { @user }
+
+  it { should respond_to(:email) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+
+  it { should be_valid }
+
+  describe "email exists and of type email" do
+    it { should validate_presence_of :email }
+    it { should allow_value('dina.ahmed@trianglz.com').for(:email) }
+    it { should_not allow_value('dina.ahmedtrianglz.com').for(:email) }
+  end
+
+  describe "when email is not present" do
+    before { @user.email = " " }
+    it { should_not be_valid }
+  end
+
+  describe "if password and confirm password doesnt match" do
+    before { @user.password = "123" }
+    it { should_not be_valid }
+  end
+
+  # describe "test name doesnt exist" do 
+  #   it { should_not validate_presence_of(:name) }
+  # end 
+
+
+end
